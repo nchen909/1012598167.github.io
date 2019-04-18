001c26dfdc8124806837405e2630a3c733128b68
1012598167 <40982544+1012598167@users.noreply.github.com>
2019-04-18 10:33:39 +0800

    2019.4.18 10:33
    change index.html

---
+++ for_array_try.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<a href="test.php">点击</a>
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenn
+ * Date: 2019/4/17
+ * Time: 21:35
+ */
+echo '123';
+$people=array("Alice","Bob");
+echo $people[1];
+foreach ($people as $person) {
+    echo $person ."";
+}
---
+++ process.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenn
+ * Date: 2019/4/17
+ * Time: 21:48
+ */
+$name=$_POST["name"];
+echo "hello,".$name;
+?>
